1da6395c769fbf821b14ea9a84af744284dcd257
NA1000APP-ROMUSNP1 <DL-1000-RPO-DEV-ROMUS DL1000RPODEVROMUS@monsanto.com>
2021-10-15 02:04:58 -0500

    build

--- pages/team/index.js
+++ pages/team/index.js
@@ -4,20 +4,20 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import PlayerList from "../../components/Players/playerList";
 
 const Team = ({ players, coaches }) => {
-  const { d1, d3, staff } = players.reduce(
-    (acc, player) => {
-      let d3 = [];
-      let d1 = [];
-      if (player.division === "D3") {
-        d3.push(player);
-      }
-      if (player.division === "D1") {
-        d1.push(player);
-      }
-      return { ...acc, d1: [...d1, ...acc.d1], d3: [...d3, ...acc.d3] };
-    },
-    { d1: [], d3: [], staff: coaches }
-  );
+  const defaultObject = { d1: [], d3: [], staff: coaches || [] };
+  const { d1, d3, staff } = players.length
+    ? players?.reduce((acc, player) => {
+        let d3 = [];
+        let d1 = [];
+        if (player.division === "D3") {
+          d3.push(player);
+        }
+        if (player.division === "D1") {
+          d1.push(player);
+        }
+        return { ...acc, d1: [...d1, ...acc.d1], d3: [...d3, ...acc.d3] };
+      }, defaultObject)
+    : defaultObject;
 
   return (
     <Layout header="Players" margin>
